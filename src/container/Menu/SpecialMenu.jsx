6dc6b46eebabfa2a99dc3285ae5a626c84111447
clark <clark_mitchelljr@hotmail.com>
2023-03-31 14:35:11 -0700

    laurels component added

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { images, data } from '../../constants';
 import './SpecialMenu.css';
+import { images, data } from '../../constants';
 import { SubHeading, MenuItem } from '../../components';
 
 const SpecialMenu = () => (
